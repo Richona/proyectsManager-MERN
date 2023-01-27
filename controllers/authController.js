e1658a002cf49c0251506209b4135d7904a2d01d
Richona <105828822+Richona@users.noreply.github.com>
2023-01-27 15:52:14 -0300

    mongoDB connected

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,14 @@
 module.exports = {
     register : async (req,res) => {
         try {
+            const {name,email,password} = req.body;
+
+            if([name,email,password].includes("") || [name,email,password].includes(null)){
+                let error = new Error("Todos los campos son obligatorios");
+                error.status = 400;
+                throw error
+            };
+
             return res.status(201).json({
                 ok : true,
                 msg :'Usuario Registrado'
