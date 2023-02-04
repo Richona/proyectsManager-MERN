11276579044518753867ff6fbc2cf71f820349a0
Richona <105828822+Richona@users.noreply.github.com>
2023-02-04 20:50:08 -0300

    change logo

--- client/src/components/root/TopBar/Logo.jsx
+++ client/src/components/root/TopBar/Logo.jsx
@@ -1,23 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { LogoHeader } from '../../../assets/LogoHeader'
 
 export const Logo = () => {
     return (
         <div className="flex flex-shrink-0 items-center">
             <Link to={"/"}>
-                <img 
-                    className="block h-8 w-auto lg:hidden" 
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" 
-                    alt="Your Company" 
-                />
-            </Link>
-            
-            <Link to={"/"}> 
-                <img 
-                    className="hidden h-8 w-auto lg:block ml-2" 
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" 
-                    alt="Your Company" 
-                />
+                <LogoHeader />
             </Link>
         </div>
     )
