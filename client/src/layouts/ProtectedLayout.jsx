8eed80b965482a185ac43eac1e6583145b2006ae
Richona <105828822+Richona@users.noreply.github.com>
2023-02-04 10:32:32 -0300

    add spinner in ProtectedLayout

--- client/src/layouts/ProtectedLayout.jsx
+++ client/src/layouts/ProtectedLayout.jsx
@@ -2,25 +2,33 @@ import { Navigate, Outlet } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 
 export const ProtectedLayout = () => {
-    const {auth, loading} = useAuth()
+    const { auth, loading } = useAuth()
 
     {
-       if(loading){
-        return <p>Cargando...</p>
-       }  
-    }
-    
-    return (
-        <>
-        {
-            auth._id ? (
-                <Outlet/>
-            ):
-            (
-                <Navigate to="/login" />
+        if (loading) {
+            return (
+                <div 
+                    className="animate-spin inline-block w-10 h-10 mt-52 border-[3px] border-current border-t-transparent text-purple-600 rounded-full" 
+                    role="status" 
+                    aria-label="loading"
+                >
+                    <span className="sr-only">Loading...</span>
+                </div>
             )
         }
+    }
+
+    return (
+        <>
+            {
+                auth._id ? (
+                    <Outlet />
+                ) :
+                    (
+                        <Navigate to="/login" />
+                    )
+            }
         </>
     )
-    
+
 }
