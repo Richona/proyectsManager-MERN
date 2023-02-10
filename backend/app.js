0fb970d3af09336e24ff7b48bf5747c20dc58c64
Richona <105828822+Richona@users.noreply.github.com>
2023-02-10 19:42:59 -0300

    vercel x3

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const app = express();
 const cors = require("cors");
 const checkToken = require('./middlewares/checkToken');
 
-const whiteList = [process.env.URL_FRONTEND, process.env.URL_FRONTEND2]
+const whiteList = [process.env.URL_FRONTEND, process.env.URL_FRONTEND2, process.env.URL_PRODUCTION]
 const corsOptions = {
   origin: function (origin, cb) {
     if (whiteList.includes(origin)) {
